553d3b04fca73edfeef554b5c508d50b5f68aaaa
Tom - SinceAway <tomfosse44@gmail.com>
2023-04-30 15:32:53 +0200

    Adding block of ruby + crafting and un-crafting of it

--- src/main/java/fr/composite/compositemc/util/RegistryHandler.java
+++ src/main/java/fr/composite/compositemc/util/RegistryHandler.java
@@ -2,7 +2,11 @@ package fr.composite.compositemc.util;
 
 
 import fr.composite.compositemc.CompositeMod;
+import fr.composite.compositemc.blocks.BlockItemBase;
+import fr.composite.compositemc.blocks.RubyBlock;
 import fr.composite.compositemc.items.ItemBase;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -12,14 +16,22 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class RegistryHandler {
 
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, CompositeMod.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, CompositeMod.MOD_ID);
 
     public static void init() {
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     // Items
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
     public static final RegistryObject<Item> TANNED_LEATHER = ITEMS.register("tanned_leather", ItemBase::new);
 
+
+    //Blocks
+    public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
+
+    //Block Items
+    public static final RegistryObject<Item> RUBY_BLOCK_ITEM = ITEMS.register("ruby_block", () -> new BlockItemBase(RUBY_BLOCK.get()));
 
 }
